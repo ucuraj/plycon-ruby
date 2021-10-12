47afc28775d970ba54bb071039109f694fc0980f
ucuraj <ulisescurajauregui@gmail.com>
2021-10-12 00:03:09 -0300

    add bar

--- lib/polycon/helpers/text.rb
+++ lib/polycon/helpers/text.rb
@@ -8,6 +8,11 @@ module Polycon
       def self.snake_to_spaced(string)
         string.gsub(/(_)/, ' ')
       end
+
+      def self.bar
+        a = [[1.23, 5, :bagels], [3.14, 7, :gravel], [8.33, 11, :saturn]]
+        '-' * (a[0][0].to_s.length + 4 + a[0][1].to_s.length + 3 + a[0][2].to_s.length + 5)
+      end
     end
   end
 end
