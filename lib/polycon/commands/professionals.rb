f45bf4b47453d5886f3f448c1b0ca538ff896390
ucuraj <ulisescurajauregui@gmail.com>
2021-10-11 21:32:28 -0300

    fix Polycon autoloads

--- lib/polycon/commands/professionals.rb
+++ lib/polycon/commands/professionals.rb
@@ -1,7 +1,5 @@
 module Polycon
   module Commands
-    autoload :Professional, 'polycon/models/professional'
-
     module Professionals
       class Create < Dry::CLI::Command
         desc 'Create a professional'
@@ -15,9 +13,9 @@ module Polycon
 
         def call(name:, **)
           begin
-            Professional.new(name).save
+            Polycon::Models::Professional.new(name).save
             puts "The professional has been successfully saved to Polycon"
-          rescue Professional::Exists => e
+          rescue Polycon::Models::Professional::Exists => e
             warn e.to_s
           end
         end
@@ -34,7 +32,7 @@ module Polycon
                 ]
 
         def call(name: nil)
-          Professional.delete(name)
+          Polycon::Models::Professional.delete(name)
         end
       end
 
@@ -46,7 +44,7 @@ module Polycon
                 ]
 
         def call(*)
-          Professional.list
+          Polycon::Models::Professional.list
         end
       end
 
@@ -61,7 +59,7 @@ module Polycon
                 ]
 
         def call(old_name:, new_name:, **)
-          Professional.rename(old_name, new_name)
+          Polycon::Models::Professional.rename(old_name, new_name)
         end
       end
     end
--- lib/polycon/models/appointment.rb
+++ lib/polycon/models/appointment.rb
@@ -1,13 +1,25 @@
-class Appointment
+module Polycon
+  module Models
+    class Appointment
+      include Polycon::Modules::FileManager
 
-  def initialize(patient_first_name, patient_last_name, patient_phone, professional, date, observations = "")
-    # variables
-    @patient_first_name = patient_first_name
-    @patient_last_name = patient_last_name
-    @patient_phone = patient_phone
-    @professional = professional
-    @date = date
-    @observations = observations
+      PROFESSIONAL_DATA_FILE_EXT = ".paf"
+
+      attr_accessor :patient_first_name, :patient_last_name, :patient_phone, :professional, :date, :observations
+
+      def initialize(patient_first_name, patient_last_name, patient_phone, professional_name, date, observations = "")
+        # variables
+        begin
+        @patient_first_name = patient_first_name
+        @patient_last_name = patient_last_name
+        @patient_phone = patient_phone
+        @professional = Polycon::Models::Professional.search(professional_name)
+        @date = date
+        @observations = observations
+        rescue Polycon::Models::Professional::NotFound => e
+          warn "Cannot create Appointment. #{e}"
+        end
+      end
+    end
   end
-
 end
